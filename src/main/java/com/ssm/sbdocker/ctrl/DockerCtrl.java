c554675341a3708b7d86d49d37a9857a970a0ec1
SSM\SHUBHAM MOKASHI <ssmokashi2000@gmail.com>
2025-03-29 00:40:59 +0530

    post method added

--- src/main/java/com/ssm/sbdocker/ctrl/DockerCtrl.java
+++ src/main/java/com/ssm/sbdocker/ctrl/DockerCtrl.java
@@ -2,6 +2,8 @@ package com.ssm.sbdocker.ctrl;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +15,9 @@ public class DockerCtrl {
 	public ResponseEntity<String> testEndpoint() {
 		return ResponseEntity.ok("Hit Success...");
 	}
-}
+	
+	@PostMapping("/body")
+	public ResponseEntity<Object> testPostEndpoint(@RequestBody Object object){
+		return ResponseEntity.ok(object);
+	}
+ }
